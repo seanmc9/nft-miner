f62b4b5a8bc858e9e9e3f17e5b69e63f5dc9c092
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-07-22 23:12:14 -0400

    chore: rename and imports

--- src/NftMiner.sol
+++ src/NftMiner.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
-contract Counter {
+import "@openzeppelin/token/ERC721/ERC721.sol";
+import "@openzeppelin/token/ERC20/ERC20.sol";
+
+contract NftMiner is ERC20 {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
