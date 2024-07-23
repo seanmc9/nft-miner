98dca6e2c6360c32575862b2399655733f7834d6
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-07-22 23:38:14 -0400

    feat: add minting logic and catching

--- src/NftMiner.sol
+++ src/NftMiner.sol
@@ -1,17 +1,31 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/token/ERC721/ERC721.sol";
+import "@openzeppelin/token/ERC721/IERC721.sol";
 import "@openzeppelin/token/ERC20/ERC20.sol";
 
 contract NftMiner is ERC20 {
-    uint256 public number;
+    IERC721 public underlyingNft;
+    mapping(uint256 => uint256) isMining;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    error MustOwnTokenId(uint256 tokenId);
+    error TokenMustBeMining();
+
+    constructor(string memory name_, string memory symbol_, IERC721 underlyingNft_) ERC20(name_, symbol_) {
+        underlyingNft = underlyingNft_;
     }
 
-    function increment() public {
-        number++;
+    function mine(uint256 tokenId) public {
+        if (underlyingNft.ownerOf(tokenId) != msg.sender) revert MustOwnTokenId(tokenId);
+
+        isMining[tokenId] = block.timestamp;
+    }
+
+    function stopMining(uint256 tokenId) public {
+        if (underlyingNft.ownerOf(tokenId) != msg.sender) revert MustOwnTokenId(tokenId);
+        if (isMining[tokenId] == 0) revert TokenMustBeMining();
+
+        _mint(msg.sender, block.timestamp - isMining[tokenId]);
+        isMining[tokenId] = 0;
     }
 }
